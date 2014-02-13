4353885b581272d20f01a36d2626407e2386e9e1
Glenn Hill <d.glenn.hill@gmail.com>
2014-02-13 09:50:39 -0500

    refactoring

--- sparky/data_access/tests/unit/backup/backup_data_access_tests.py
+++ sparky/data_access/tests/unit/backup/backup_data_access_tests.py
@@ -1,5 +1,5 @@
 from sparky.data_access.backup.backup_data_access import backup_data_access
-from mock_data_store import mock_data_store
+from sparky.data_access.tests.common.mock_data_store import mock_data_store
 
 ds = mock_data_store()
 bds = backup_data_access(ds)
